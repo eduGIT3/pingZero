244a3df03a2712f15e725c75585c707de0b91573
DavidAlonsoDev <davidalonsofer@gmail.com>
2026-05-20 10:18:54 +0200

    Initial commit

---
+++ backend/src/main/java/com/example/pingZero/PingZeroApplication.java
@@ -0,0 +1,13 @@
+package com.example.pingZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingZeroApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/pingZero/PingZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pingZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
